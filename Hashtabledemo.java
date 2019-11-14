0564082fe9aa74dfb10c358cfc4823cefc1f6821
Aanuragkumarsrivastava <42467453+Aanuragkumarsrivastava@users.noreply.github.com>
2019-11-14 14:39:17 +0530

    added program on hashtable

---
+++ Hashtabledemo.java
@@ -0,0 +1,28 @@
+package pack2;
+
+import java.util.Hashtable;
+
+public class Hashtabledemo {
+
+	public static void main(String[] args) {
+
+				
+				
+				Hashtable<Integer, String> map =
+						new Hashtable<Integer, String>();
+				map.put(34, "tom");
+				map.put(27, "jerry");
+				map.put(78, "miller");
+				map.put(54, "watson");
+				map.put(62, "arthur");
+				//map.put(null, "test");
+				
+				
+				
+				System.out.println(map);
+
+			}
+
+	}
+
+
